1e484c15dee722630458b7125845a5d94e1c0706
juniorsaraviao <junior.saravia.o@uni.pe>
2021-12-05 19:17:22 -0500

    Customize Top Header

--- app/src/main/java/com/test/jettipapp/MainActivity.kt
+++ app/src/main/java/com/test/jettipapp/MainActivity.kt
@@ -3,9 +3,7 @@ package com.test.jettipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,8 +11,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.test.jettipapp.ui.theme.JetTipAppTheme
@@ -25,7 +26,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Text(text = "Hello Again!")
+                TopHeader()
             }
         }
     }
@@ -43,13 +44,22 @@ fun MyApp( content: @Composable () -> Unit ) {
 
 @Preview
 @Composable
-fun TopHeader() {
-    Surface( modifier = Modifier.fillMaxWidth()
+fun TopHeader( totalPerPerson: Double = 134.0 ) {
+    Surface( modifier = Modifier
+                        .fillMaxWidth()
                         .height(150.dp)
                         //.clip(shape = RoundedCornerShape(12.dp))
-                        .clip(shape = CircleShape.copy(all = CornerSize(12.dp)))) {
-        Column() {
-
+                        .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+                             color = Color(0xFFE9D7F7)
+    ) {
+        Column( modifier = Modifier.padding(12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center ) {
+            Text( text = "Total Per Person",
+                  style = MaterialTheme.typography.h5 )
+            Text( text = "$${String.format("%.2f", totalPerPerson)}",
+                  style = MaterialTheme.typography.h4,
+                  fontWeight = FontWeight.ExtraBold )
         }
     }
 }
