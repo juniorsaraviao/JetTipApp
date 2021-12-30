06e557daa7cc607fc5c9948b4c5085d27402c1c5
juniorsaraviao <junior.saravia.o@uni.pe>
2021-12-29 23:34:30 -0500

    Add tip and slider

--- app/src/main/java/com/test/jettipapp/MainActivity.kt
+++ app/src/main/java/com/test/jettipapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Slider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -108,6 +109,10 @@ fun BillForm( modifier: Modifier = Modifier,
 
     val keyboardController = LocalSoftwareKeyboardController.current
 
+    val sliderPositionState = remember {
+        mutableStateOf(0f)
+    }
+
     Surface( modifier = Modifier
                         .padding(2.dp)
                         .fillMaxWidth(),
@@ -150,6 +155,26 @@ fun BillForm( modifier: Modifier = Modifier,
                             onClick = { splitNumber.value++ } )
                     }
                 }
+
+                // Tip row
+                Row( modifier = Modifier.padding( horizontal = 3.dp, vertical = 12.dp ) ) {
+                    Text( text = "Tip",
+                          modifier = Modifier.align( alignment = Alignment.CenterVertically ) )
+                    Spacer( modifier = Modifier.width(200.dp) )
+                    Text( text = "$33.00",
+                          modifier = Modifier.align( alignment = Alignment.CenterVertically ) )
+                }
+
+                Column( verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally ) {
+                    Text( text = "33%" )
+                    Spacer( modifier = Modifier.height(14.dp) )
+                    Slider( value = sliderPositionState.value, onValueChange = { newVal ->
+                        Log.d("Slider", "BillForm: $newVal")
+                        sliderPositionState.value = newVal
+                    } )
+                }
+
             }else{
                 Box() {}
             }
